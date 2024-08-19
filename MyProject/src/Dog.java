e9c64caf5a82bc1a886fdc3ec9ba98dae6094fe0
Abdelaziz shahin <abd91business@gmail.com>
2024-08-19 02:24:22 +0200

    testing methods

--- MyProject/src/Dog.java
+++ MyProject/src/Dog.java
@@ -3,13 +3,15 @@ public class Dog extends Animal{
     private String earShape;
     private String tailShape;
 
-    public Dog(String type, String size, double weight) {
-        super(type, size, weight);
+    public Dog() {
+        super("doberman","big",50);
+    }
+    public Dog (String type, double weight){
+        this(type,weight,"Perky","Curled");
     }
 
 
-
-    public Dog(String type, String size, double weight, String earShape, String tailShape) {
+    public Dog(String type,   double weight, String earShape, String tailShape) {
         super(type, weight < 15 ? "small" :(weight<35 ? "medium" : "big"),weight);
         this.earShape = earShape;
         this.tailShape = tailShape;
--- MyProject/src/Main.java
+++ MyProject/src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog("staff", "small", 14);
-        System.out.println(dog);
-        Dog dog2 = new Dog("staff", "small", 14, "curve", "straight");
-        System.out.println(dog2);
-        Animal animal = new Animal("dog", "small", 14);
+        Dog dogOne =new Dog("Hasky",25);
+        Dog dogTwo = new Dog("Staff",17,"curve","twisted");
+        Animal animal = new Animal("doberman","big",30);
+
+        dogOne.makeNoise();
+        dogOne.move("fast");
+        dogTwo.move("slow");
         animal.makeNoise();
-        dog.move("fast");
     }
 }
